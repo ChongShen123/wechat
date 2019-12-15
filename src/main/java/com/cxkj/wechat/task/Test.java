97088ceed775d1c2087e8f754dfab13b8a22b3d4
anyetanxi2017 <tiankongalyssa@outlook.com>
2019-12-15 18:23:48 +0800

    test

---
+++ src/main/java/com/cxkj/wechat/task/Test.java
@@ -0,0 +1,11 @@
+package com.cxkj.wechat.task;
+
+/**
+ * @author tiankong
+ * @date 2019/12/15 18:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
