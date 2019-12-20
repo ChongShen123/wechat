7a56ae1c13e9d235d97b7c0be57297f75923d61e
anyetanxi2017 <tiankongalyssa@outlook.com>
2019-12-20 15:51:13 +0800

    update 15:51

--- src/main/java/com/cxkj/wechat/entity/GroupChat.java
+++ src/main/java/com/cxkj/wechat/entity/GroupChat.java
@@ -14,7 +14,6 @@ import java.io.Serializable;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
-@Document(collation = "GroupChat")
 public class GroupChat extends ChatInfo implements Serializable {
     private Integer toGroupId;
 
