801fee35dbf6df6652d5e495b27e2ffbc9494994
anyetanxi2017 <tiankongalyssa@outlook.com>
2020-01-07 19:12:10 +0800

    群聊功能检查完毕

--- wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/JoinGroupCmd.java
+++ wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/JoinGroupCmd.java
@@ -1,5 +1,6 @@
 package com.xsdkj.wechat.netty.cmd.group;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.xsdkj.wechat.common.Cmd;
 import com.xsdkj.wechat.common.JsonResult;
@@ -8,16 +9,20 @@ import com.xsdkj.wechat.ex.GroupNotFoundException;
 import com.xsdkj.wechat.ex.UserJoinedException;
 import com.xsdkj.wechat.netty.cmd.CmdAnno;
 import com.xsdkj.wechat.netty.cmd.base.AbstractChatCmd;
+import com.xsdkj.wechat.util.LogUtil;
 import com.xsdkj.wechat.util.SessionUtil;
 import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Set;
 
 /**
  * @author tiankong
  * @date 2019/12/15 18:55
  */
+@Slf4j
 @CmdAnno(cmd = Cmd.JOIN_GROUP)
 @Service
 public class JoinGroupCmd extends AbstractChatCmd {
@@ -29,30 +34,40 @@ public class JoinGroupCmd extends AbstractChatCmd {
 
     @Override
     protected void concreteAction(Channel channel) {
+        long begin = System.currentTimeMillis();
+        log.debug("开始处理加入群聊...");
         Set<Integer> ids = requestParam.getIds();
         Integer groupId = requestParam.getGroupId();
+        log.debug("要加入群组的用户id{},群组id:{} {}ms", ids, groupId, DateUtil.spendMs(begin));
         // 判断群是否存在
         UserGroup group = groupService.getGroupById(groupId);
-        if (group == null) {
-            throw new GroupNotFoundException();
-        }
-        if (checkGroupUserJoined(ids, requestParam.getGroupId())) {
+        if (group != null) {
+            if (!checkGroupUserJoined(ids, requestParam.getGroupId())) {
+                ids.forEach(uid -> {
+                    Channel userChannel = SessionUtil.getUserChannel(uid);
+                    if (userChannel != null) {
+                        SessionUtil.joinGroup(groupId, userChannel);
+                    }
+                });
+                //
+                groupService.insertUserIds(ids, groupId);
+                log.debug("已保存用户与群组关系到数据库 {}ms", DateUtil.spendMs(begin));
+                // 群成员个数增加
+                groupService.updateGroupCount(ids.size(), groupId);
+                log.debug("已更新群成员个数 {}ms", DateUtil.spendMs(begin));
+                //给用户发送一个入群消息,保存到数据库
+                sendCreateGroupMessageToUsers(ids, group);
+                // 更新群组redis缓存
+                groupService.updateRedisGroupById(groupId);
+                sendMessage(channel, JsonResult.success(cmd));
+                log.debug("加入群组处理完成 {}ms", DateUtil.spendMs(begin));
+                log.debug(LogUtil.INTERVAL);
+                return;
+            }
+            log.error("有用户已加入群组 {}ms", DateUtil.spendMs(begin));
             throw new UserJoinedException();
         }
-        ids.forEach(uid -> {
-            Channel userChannel = SessionUtil.getUserChannel(uid);
-            if (userChannel != null) {
-                SessionUtil.joinGroup(groupId, userChannel);
-            }
-        });
-        // 保存用户与群组关系
-        groupService.insertUserIds(ids, groupId);
-        // 群成员个数增加
-        groupService.updateGroupCount(ids.size(), groupId);
-        //给用户发送一个入群消息,保存到数据库
-        sendCreateGroupMessageToUsers(ids, group);
-        // 更新群组redis缓存
-        groupService.updateRedisGroupById(groupId);
-        sendMessage(channel, JsonResult.success(cmd));
+        log.error("{}群不存在 {}ms", groupId, DateUtil.spendMs(begin));
+        throw new GroupNotFoundException();
     }
 }
--- wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/ListMembersCmd.java
+++ wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/ListMembersCmd.java
@@ -1,5 +1,6 @@
 package com.xsdkj.wechat.netty.cmd.group;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.xsdkj.wechat.common.Cmd;
 import com.xsdkj.wechat.common.JsonResult;
@@ -7,6 +8,7 @@ import com.xsdkj.wechat.netty.cmd.CmdAnno;
 import com.xsdkj.wechat.netty.cmd.base.AbstractChatCmd;
 import com.xsdkj.wechat.vo.ListMembersVo;
 import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.List;
  * @author tiankong
  * @date 2019/12/17 11:12
  */
+@Slf4j
 @Service
 @CmdAnno(cmd = Cmd.LIST_GROUP_MEMBERS)
 public class ListMembersCmd extends AbstractChatCmd {
@@ -25,10 +28,12 @@ public class ListMembersCmd extends AbstractChatCmd {
 
     @Override
     protected void concreteAction(Channel channel) {
+        long begin = System.currentTimeMillis();
         Integer groupId = requestParam.getGroupId();
         List<ListMembersVo> listMembersVos = groupService.listGroupMembersByGroupId(groupId);
         if (listMembersVos.size() > 0) {
             sendMessage(channel, JsonResult.success(listMembersVos, cmd));
         }
+        log.debug("查询群成员用户完成 {}ms", DateUtil.spendMs(begin));
     }
 }
