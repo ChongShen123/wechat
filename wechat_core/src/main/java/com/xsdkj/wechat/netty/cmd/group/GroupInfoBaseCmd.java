246d5081786bfbb78530e82b886e092a4a75480c
anyetanxi2017 <tiankongalyssa@outlook.com>
2020-01-07 18:31:15 +0800

    查看群详情检查完毕

--- wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/GroupInfoBaseCmd.java
+++ wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/group/GroupInfoBaseCmd.java
@@ -1,5 +1,6 @@
 package com.xsdkj.wechat.netty.cmd.group;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.xsdkj.wechat.common.Cmd;
 import com.xsdkj.wechat.common.JsonResult;
@@ -8,12 +9,14 @@ import com.xsdkj.wechat.netty.cmd.CmdAnno;
 import com.xsdkj.wechat.netty.cmd.base.AbstractChatCmd;
 import com.xsdkj.wechat.vo.GroupBaseInfoVo;
 import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author tiankong
  * @date 2019/12/15 13:58
  */
+@Slf4j
 @CmdAnno(cmd = Cmd.GROUP_BASE_INFO)
 @Service
 public class GroupInfoBaseCmd extends AbstractChatCmd {
@@ -25,11 +28,13 @@ public class GroupInfoBaseCmd extends AbstractChatCmd {
 
     @Override
     protected void concreteAction(Channel channel) {
+        long begin = System.currentTimeMillis();
         GroupBaseInfoVo groupBaseInfoVO = groupService.getBaseInfo(requestParam.getGroupId());
         if (groupBaseInfoVO != null) {
             sendMessage(channel, JsonResult.success(groupBaseInfoVO, cmd));
         } else {
             sendMessage(channel, JsonResult.failed(ResultCodeEnum.DATA_NOT_EXIST, cmd));
         }
+        log.debug("查看群基本信息处理完成 {}ms", DateUtil.spendMs(begin));
     }
 }
