9d431917734cc0de51fd1997bdace38500b6f73d
anyetanxi2017 <tiankongalyssa@outlook.com>
2020-01-08 13:48:14 +0800

    查询用户详情检查完成

--- wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/user/UserInfoCmd.java
+++ wechat_core/src/main/java/com/xsdkj/wechat/netty/cmd/user/UserInfoCmd.java
@@ -1,5 +1,6 @@
 package com.xsdkj.wechat.netty.cmd.user;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.xsdkj.wechat.common.Cmd;
 import com.xsdkj.wechat.common.JsonResult;
@@ -12,13 +13,18 @@ import com.xsdkj.wechat.ex.DataEmptyException;
 import com.xsdkj.wechat.ex.PermissionDeniedException;
 import com.xsdkj.wechat.vo.UserVo;
 import io.netty.channel.Channel;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * @author tiankong
  * @date 2019/12/30 14:39
  */
+@Slf4j
 @Component
 @CmdAnno(cmd = Cmd.USER_INFO)
 public class UserInfoCmd extends AbstractChatCmd {
@@ -29,16 +35,23 @@ public class UserInfoCmd extends AbstractChatCmd {
 
     @Override
     protected void concreteAction(Channel channel) throws RuntimeException {
+        long begin = System.currentTimeMillis();
         Integer userId = requestParam.getUserId();
         User user = userService.getRedisUserByUserId(userId);
-        if (user == null) {
+        try {
+            if (user != null) {
+                if (user.getPlatformId().equals(session.getPlatformId()) && session.getType().equals(UserConstant.TYPE_ADMIN)) {
+                    UserVo userVo = new UserVo();
+                    BeanUtils.copyProperties(user, userVo);
+                    sendMessage(channel, JsonResult.success(userVo, cmd));
+                    return;
+                }
+                log.error("用户{}没有相关权限", session.getUid());
+                throw new PermissionDeniedException();
+            }
             throw new DataEmptyException();
+        } finally {
+            log.debug("查询用户{}业务处理完成 {}ms",userId, DateUtil.spendMs(begin));
         }
-        if (!user.getPlatformId().equals(session.getPlatformId()) || user.getType().equals(UserConstant.TYPE_USER)) {
-            throw new PermissionDeniedException();
-        }
-        UserVo userVo = new UserVo();
-        BeanUtils.copyProperties(user, userVo);
-        sendMessage(channel, JsonResult.success(userVo, cmd));
     }
 }
